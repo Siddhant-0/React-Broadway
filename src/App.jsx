1b0a31d221be503f95c9f07959cfc2c52c7dbaf4
Siddhant Shrestha <siddhantshrestha04@gmail.com>
2024-12-02 21:08:17 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Button from "./button";
+
+import Animal from "./Animal";
+
+const App=()=>{
+  return (
+    <section>
+      <p>Welcome to my world</p>
+      <a href="https:\\www.google.com">Google</a>
+      <Button/>
+      <Animal/>
+    </section>
+  );
+}
+
+export default App; 
---
+++ src/Button.jsx
@@ -0,0 +1,15 @@
+const buttonStyle={ 
+    padding :"10px",
+    background : "blue",
+    color: "black",
+    border:"none",
+    cursor : "pointer"
+
+}
+
+const Button=()=>{
+    return <button style={buttonStyle}>Learn More</button>
+    
+}
+
+export default Button;
